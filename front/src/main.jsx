244b7e5eeeaee71b18515651a4ddcb8720b916b8
Baptiste Loison <baptisteloison8400@gmail.com>
2023-03-16 15:11:02 +0100

    feat: add Text page

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -3,11 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./pages/App";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Test from "./pages/Test";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/test",
+    element: <Test />,
   },
 ]);
 
---
+++ front/src/pages/Test.jsx
@@ -0,0 +1,9 @@
+export default function Test() {
+  return (
+    <>
+      <h1>Bienvenue sur la page de Test</h1>
+      <h3>Ici vous pouvez modifier totalement la page pour vos test</h3>
+      <h3>Mais attention ne surtout pas commit Test.jsx ni Test.css</h3>
+    </>
+  );
+}
