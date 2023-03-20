cb0225b39bfe6a5f57ddbd9ef524171e7f5ca764
Youbuze <tom.baillet2@gmail.com>
2023-03-20 22:27:14 +0100

    add modif on page

--- front/src/pages/Notif.jsx
+++ front/src/pages/Notif.jsx
@@ -1,19 +1,89 @@
-import { useState } from 'react';
-import classes from './Register.module.css';
-import Input from '../components/Input/Input';
-import Button from '../components/Button/Button';
 import Navbar from '../components/Navbar/Navbar';
-import Icon from '../components/Icon/Icon';
-import Poste from '../components/Poste/Poste';
 import HeaderProfil from "../components/Header-profil/Header-profil";
+import Notifications from '../components/Notifications/Notifications';
+import "./notif.css"
 
 export default function Notif(props) {
+  const Notif = [
+    {
+      author: "@Youbuze",
+      time: "15/05/2001",
+      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis libero dolorum ad.",
+    },
+    {
+      author: "@Youbuze",
+      time: "15/05/2001",
+      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis libero dolorum ad.",
+    },
+    {
+      author: "@Youbuze",
+      time: "15/05/2001",
+      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis libero dolorum ad.",
+    },
+    {
+      author: "@Youbuze",
+      time: "15/05/2001",
+      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis libero dolorum ad.",
+    },
+    {
+      author: "@Youbuze",
+      time: "15/05/2001",
+      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis libero dolorum ad.",
+    },
+    {
+      author: "@Youbuze",
+      time: "15/05/2001",
+      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis libero dolorum ad.",
+    },
+    {
+      author: "@Youbuze",
+      time: "15/05/2001",
+      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis libero dolorum ad.",
+    },
+    {
+      author: "@Youbuze",
+      time: "15/05/2001",
+      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis libero dolorum ad.",
+    },
+    {
+      author: "@Youbuze",
+      time: "15/05/2001",
+      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis libero dolorum ad.",
+    },
+    {
+      author: "@Youbuze",
+      time: "15/05/2001",
+      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis libero dolorum ad.",
+    },
+    {
+      author: "@Youbuze",
+      time: "15/05/2001",
+      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis libero dolorum ad.",
+    },
+    {
+      author: "@Youbuze",
+      time: "15/05/2001",
+      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis libero dolorum ad.",
+    },
+  ]
+
   return (
     <>
         <Navbar isConnected={true}/>
         <div className="container">
             <HeaderProfil username="@Youbuze" nbPoste="12" nbFollow="1212" nbFollower="1"/>
-            
+            <div className="notif-container">
+              {Notif.map((notifications, index) => (
+                <Notifications
+                  key={index}
+                  content={notifications.content}
+                  author={notifications.author}
+                  time={notifications.time}
+                  type={notifications.type}
+                  link={notifications.link}
+                />
+              ))}
+            </div>
         </div>
     </>
   );
