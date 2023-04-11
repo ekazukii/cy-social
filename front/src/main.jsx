ca8858f18aa2608ab53ccb4ed2a887af50065fe6
Baptiste Loison <baptisteloison8400@gmail.com>
2023-04-11 22:11:47 +0200

    feat: start session management for back&front

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,57 +1,60 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./pages/App";
-import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Test from "./pages/Test";
-import RegisterPage from "./pages/Register";
-import Profil from "./pages/Profil";
-import Post from "./pages/Poste";
-import Message from "./pages/Message"
-import Notif from "./pages/Notif";
-import Accueil from "./pages/Accueil";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './pages/App';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Test from './pages/Test';
+import RegisterPage from './pages/Register';
+import Profil from './pages/Profil';
+import Post from './pages/Poste';
+import Message from './pages/Message';
+import Notif from './pages/Notif';
+import Accueil from './pages/Accueil';
+import { SessionProvider } from './hooks/useSession';
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Accueil />,
+    path: '/',
+    element: <Accueil />
   },
   {
-    path: "/App",
-    element: <App />,
+    path: '/App',
+    element: <App />
   },
   {
-    path: "/test",
-    element: <Test />,
+    path: '/test',
+    element: <Test />
   },
   {
-    path: "/register",
-    element: <RegisterPage />,
+    path: '/register',
+    element: <RegisterPage />
   },
   {
-    path: "/profil",
-    element: <Profil info="with-post"/>,
+    path: '/profil',
+    element: <Profil info="with-post" />
   },
   {
-    path: "/profil/with-replies",
-    element: <Profil info="with-replies"/>,
+    path: '/profil/with-replies',
+    element: <Profil info="with-replies" />
   },
   {
-    path: "/post/:id",
-    element: <Post />,
+    path: '/post/:id',
+    element: <Post />
   },
   {
-    path: "/messagerie",
-    element: <Message />,
+    path: '/messagerie',
+    element: <Message />
   },
   {
-    path: "/notif",
-    element: <Notif />,
+    path: '/notif',
+    element: <Notif />
   }
 ]);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <SessionProvider>
+      <RouterProvider router={router} />
+    </SessionProvider>
   </React.StrictMode>
 );
