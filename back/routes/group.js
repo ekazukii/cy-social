beff5bcbf4c05aff14c3e6b3b8d52551a72cec87
Baptiste Loison <baptisteloison8400@gmail.com>
2023-04-11 14:57:29 +0200

    feat: add post and group endpoints

--- back/routes/group.js
+++ back/routes/group.js
@@ -1,10 +1,25 @@
 var express = require('express');
 var router = express.Router();
+const { createGroup, deleteGroup, updateGroup, getGroup, getGroupsOf } = require('../models/group');
 
-router.get('/', function (req, res) {});
+router.get('/', async function (req, res) {
+  const { user } = req.query;
+  if (typeof user !== 'string') return res.status(400).send({ error: true });
+
+  const groups = await getGroupsOf(Number(user));
+  return res.status(200).send({ error: false, groups });
+});
+
+router.get('/:id', async function (req, res) {
+  const { id } = req.params;
+  const group = await getGroup(id);
+  return res.status(200).send({ error: false, group });
+});
 
 router.post('/', function (req, res) {});
 
 router.put('/', function (req, res) {});
 
 router.delete('/', function (req, res) {});
+
+module.exports = router;
