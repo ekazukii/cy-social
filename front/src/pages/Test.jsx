cd6e16a5b1049eec30f895427c60d77862140aba
Youbuze <tom.baillet2@gmail.com>
2023-04-12 10:50:58 +0200

    Ajout page Accueil + Messagerie + Post detail

--- front/src/pages/Test.jsx
+++ front/src/pages/Test.jsx
@@ -1,27 +1,7 @@
-import CreatePoste from "../components/Poste/CreatePoste"
-
 export default function Test() {
-  const author = 
-    {
-      "id": 1,
-      "name": "Baillet Tom",
-      "username": "@Youbuze",
-      "img" : "/img/avatar.png",
-      "nbPoste": 12,
-      "nbFollow": 13,
-      "nbFollower": 14,
-    }
-  // let [postes, setPostes] = useState(null);
-
-  // useEffect(() => {
-  //   fetch("http://localhost:3000/post")
-  //   .then(response => response.json())
-  //   .then(data => setPostes(data))
-  // },[])
-
   return (
     <>
-      <CreatePoste author={author}/>
+      <h3>Bienvenue sur la page de Test!</h3>
     </>
   );
 }
