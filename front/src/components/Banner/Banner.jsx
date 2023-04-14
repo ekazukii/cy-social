6dfc380345147ff2a48c2d28900365dc7b9d3f10
Youbuze <tom.baillet2@gmail.com>
2023-04-14 18:37:35 +0200

    Add component banner for profil page

---
+++ front/src/components/Banner/Banner.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import classes from "./banner.module.css";
+import Icon from "../Icon/Icon"
+
+/**
+ * 
+ * @param {Object} user
+ * @param {String} user.profile_pic 
+ * @param {String} user.username 
+ * @param {String} user.name 
+ * @param {Number} user.nbPosts 
+ * @param {Number} user.nbFollows 
+ * @param {Number} user.nbFollowers
+ * @returns 
+ */
+export default function HeaderProfil({user}) {
+ return (
+    <div className={classes["header-profil-container"]}>
+        <div className={classes["header-profil-top"]}>
+            <img
+                src="/img/banner.png"
+                alt={user.name}
+            />
+            <span>@{user.username}</span>
+        </div>
+        <div className={classes["header-profil-mid"]}>
+            <div className={classes["header-profil-user-pic"]}>
+                <img
+                src={user.profile_pic}
+                alt={user.name}
+                />
+            </div> 
+            <div className={classes["header-profil-user-details"]}>
+                <div className={classes["header-profil-user-details-name"]}>
+                    {user.name}
+                </div>
+                <div className={classes["header-profil-user-details-bio"]}>
+                    <span>Ajouter une bio?</span>
+                </div>
+                {/* {user.isUser ? (<Button text="Suivre" type="primary"/>) : (<Button text="Paramètres" type="primary"/>)} */}
+            </div>
+        </div>
+        <div className={classes["header-profil-bottom"]}>
+                <span>{user.nbPosts} Postes publiés</span>
+                <span>{user.nbFollows} Abonnements</span>
+                <span>{user.nbFollowers} Abonnés</span>
+        </div>
+    </div>
+  );
+}
--- front/src/pages/Profil.jsx
+++ front/src/pages/Profil.jsx
@@ -1,42 +1,73 @@
-import HeaderProfil from "../components/Header-profil/Header-profil"
-import Navbar from "../components/Navbar/Navbar"
+import HeaderProfil from "../components/Header-profil/Header-profil";
+import Banner from "../components/Banner/Banner";
+import Navbar from "../components/Navbar/Navbar";
 import Poste from "../components/Poste/Poste";
-import "./profil.css"
 import { useParams } from "react-router-dom";
 import React, { useEffect, useState } from 'react';
+import { useSession } from '../hooks/useSession';
+import classes from "./profil.module.css";
 
 export default function Profil(props) {
+  const { user, setSession, login, refreshData, logout } = useSession();
   const [data, setData] = useState([]);
+  const [dataUser, setDataUser] = useState({});
+  const [dataNotif, setDataNotif] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    fetch("http://localhost:3000/post?user=1") // remplacez l'URL par celle de votre API
-      .then(response => response.json())
-      .then(data => setData(data))
-      .catch(error => console.error(error));
+    Promise.all([
+      fetch("http://localhost:3000/post?user=4").then(response => response.json()),
+      fetch("http://localhost:3000/user/1").then(response => response.json()),
+      fetch("http://localhost:3000/notif?user=1").then(response => response.json()),
+    ]).then(([postData, userData, notifData]) => {
+      setData(postData);
+      setDataUser(userData[0]);
+      setDataNotif(notifData);
+      setIsLoading(false);
+    }).catch(error => console.error(error));
   }, []);
 
-    return (
+  const isConnected = !!user?.id;
+
+  console.log("user:", user);
+  console.log("data:", data);
+  console.log("dataUser:", dataUser);
+  console.log("dataNotif:", dataNotif);
+
+  return (
     <>
-      <Navbar />
-      <div className="container">
-        <HeaderProfil username="@Youbuze" nbPoste="12" nbFollow="1212" nbFollower="1" isUser={true}/>
-        <div className="switcher">
-            <nav>
-                <div>Postes</div>
-                <div>Commentaires</div>
-                <div>Likes</div>
-            </nav>
-            {props.info === "with-post" && 
-            <div className="list-post">
-                {data.map((item, index) => (
-                    <div key={index}>
-                        <Poste poste={item} />
-                    </div>
-                ))}
+      <Navbar isConnected={isConnected} notifs={dataNotif} />
+      {isLoading ? (
+        <div>Chargement des données...</div>
+      ) : (
+        <div className={classes["container_body"]}>
+          {/* Affichage des informations de l'utilisateur */}
+          <div className={classes["container_body_left"]}>
+            <h3>Nouveau Sondage</h3>
+            <Banner user={dataUser} />
+            <h3>Mon Recap</h3>
+            <div className={classes["recapBox"]}>
+
             </div>
-            }
+            <h3>Mes favoris</h3>
+            <div className={classes["recapBox"]}>
+
+            </div>
+          </div>
+          <div className={classes["container_body_center"]}>
+            <h3>Ma Timeline</h3>
+            {props.info === "with-post" && (
+              <div className={classes["list-post"]}>
+                {data.map((item, index) => (
+                  <Poste key={index} poste={item} user={dataUser} />
+                ))}
+              </div>
+            )}
+          </div>
+          <div className={classes["container_body_right"]}>
+          </div>
         </div>
-      </div>
+      )}
     </>
-    );
-  }
+  );
+}
