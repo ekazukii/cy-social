6bdbea329c406662f4b710227e8d7536b10775ad
Youbuze <tom.baillet2@gmail.com>
2023-04-14 16:32:41 +0200

    Recup users sur Accueil Et Poste

--- front/src/components/Header-profil/Header-profil.jsx
+++ front/src/components/Header-profil/Header-profil.jsx
@@ -3,22 +3,33 @@ import Icon from "../Icon/Icon";
 import Button from "../Button/Button";
 import classes from "./header-profil.module.css";
 
-export default function HeaderProfil(props) {
+/**
+ * 
+ * @param {Object} user
+ * @param {String} user.profile_pic 
+ * @param {String} user.username 
+ * @param {String} user.name 
+ * @param {Number} user.nbPosts 
+ * @param {Number} user.nbFollows 
+ * @param {Number} user.nbFollowers
+ * @returns 
+ */
+export default function HeaderProfil({user}) {
  return (
     <div className={classes["header-profil-container"]}>
         <div className={classes["header-profil-user"]}>
             <div className={classes["header-profil-user-pic"]}>
                 <img
-                src="/img/avatar.png"
-                alt="user"
+                src={user.profile_pic}
+                alt={user.name}
                 />
-                <span>{props.username}</span>
+                <span>{user.username}</span>
             </div> 
             <div className={classes["header-profil-user-details"]}>
-                <span>{props.nbPoste} Postes publiés</span>
-                <span>{props.nbFollow} Abonnements</span>
-                <span>{props.nbFollower} Abonné</span>
-                {props.isUser ? (<Button text="Suivre" type="primary"/>) : (<Button text="Paramètres" type="primary"/>)}
+                <span>{user.nbPosts} Postes publiés</span>
+                <span>{user.nbFollows} Abonnements</span>
+                <span>{user.nbFollowers} Abonné</span>
+                {/* {user.isUser ? (<Button text="Suivre" type="primary"/>) : (<Button text="Paramètres" type="primary"/>)} */}
             </div>
         </div>
         <div className={classes["header-profil-group"]}> 
