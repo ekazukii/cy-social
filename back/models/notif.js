f7b104adfe739cb58bbf7c3b464a1c390e5b7205
Baptiste Loison <baptisteloison8400@gmail.com>
2023-05-06 22:16:38 +0200

    feat: create notification on events

--- back/models/notif.js
+++ back/models/notif.js
@@ -1,26 +1,65 @@
-const { createMockNotif } = require('../utils/mockData');
+const { getUsersInGroup, getUser } = require('./user');
+const { getPost } = require('./post');
+const { asyncQuery } = require('./database');
+const mysql = require('mysql2');
 
-let _notifs = [createMockNotif(1), createMockNotif(2), createMockNotif(3)];
-
-/**
- * create a notification in db
- * @param {Number} userId
- */
 const getNotif = userId => {
-  if (!userId) return _notifs;
-  return _notifs.filter(notif => notif.target === userId);
+  const sql = mysql.format('SELECT * FROM Notifications WHERE id_user = ?', [userId]);
+  return asyncQuery(sql);
 };
 
 const updateNotif = (id, read) => {
-  const notif = _notifs.find(notif => notif.id === id);
-  notif.read = read;
-  return notif;
+  // will see later
 };
 
-const createNotif = () => {};
+const createNotif = (idUser, link, title, type) => {
+  const sql = mysql.format('INSERT INTO Notifications (id_user, link, title, type) VALUES(?, ?, ?, ?)', [
+    idUser,
+    link,
+    title,
+    type
+  ]);
+
+  return asyncQuery(sql);
+};
 
 const deleteNotif = id => {
-  _notifs = _notifs.filter(notif => notif.id !== id);
+  const sql = mysql.format('DELETE FROM Notifications WHERE id = ?', [id]);
+  return asyncQuery(sql);
 };
 
-module.exports = { createNotif, updateNotif, getNotif, deleteNotif };
+const generatePostNotifs = async (postId, authorId, groupId, title) => {
+  const users = await getUsersInGroup(groupId);
+  users.forEach(user => {
+    createNotif(user.id, `/post/${postId}`, title, 1);
+  });
+};
+
+// send notifcation to post author
+const generateCommentNotifs = async (postId, authorId, message) => {
+  const post = (await getPost(undefined, undefined, postId))[0];
+  const user = (await getUser(authorId))[0];
+  createNotif(post.id_user, `/post/${postId}`, user.username + ' à posté un commentaire : ' + message, 2);
+};
+
+const generateFollowerNotifs = async (userId, followerId) => {
+  const follower = (await getUser(followerId))[0];
+  createNotif(userId, `/user/${followerId}`, `${follower.name} a commencé à vous suivre`, 3);
+};
+
+const generateLikeNotifs = async (userId, postId) => {
+  const post = (await getPost(undefined, undefined, postId))[0];
+  const user = (await getUser(userId))[0];
+  createNotif(post.id_user, `/post/${postId}`, `${user.name} a aimé votre post : ${post.title}`, 4);
+};
+
+module.exports = {
+  createNotif,
+  updateNotif,
+  getNotif,
+  deleteNotif,
+  generatePostNotifs,
+  generateCommentNotifs,
+  generateFollowerNotifs,
+  generateLikeNotifs
+};
--- back/routes/post.js
+++ back/routes/post.js
@@ -5,6 +5,7 @@ const { createPost, updatePost, getPostWithCounts, deletePost, addView, getPost 
 const { createVote, updateVote, deleteVote, getVote } = require('../models/vote');
 const { createLike, getLike, updateLike, deleteLike } = require('../models/like');
 const { createComment, getComment, updateComment, deleteComment } = require('../models/comment');
+const { generatePostNotifs, generateCommentNotifs, generateLikeNotifs } = require('../models/notif');
 const { getUsers } = require('../models/user');
 
 //TODO: Check date format
@@ -31,7 +32,7 @@ router.get('/tl', async function (req, res) {
   res.send({ posts, users });
 });
 
-router.post('/', function (req, res) {
+router.post('/', async function (req, res) {
   const { authorId, title, content, description, group, img, dateEnd } = req.body;
   if (
     typeof authorId !== 'string' ||
@@ -44,7 +45,10 @@ router.post('/', function (req, res) {
   )
     return res.status(400).send({ error: true });
 
-  const post = createPost(Number(authorId), title, content, group, img, dateEnd);
+  const post = await createPost(Number(authorId), title, content, group, img, dateEnd);
+  const postId = post.insertId;
+
+  generatePostNotifs(postId, authorId, group, title);
   res.send(post);
 });
 
@@ -137,6 +141,7 @@ router.post('/like', async function (req, res) {
   }
 
   const newLike = await createLike(Number(id), Number(user), 0);
+  generateLikeNotifs(Number(user), Number(id));
   res.send(newLike);
 });
 
@@ -158,10 +163,11 @@ router.delete('/like', function (req, res) {
 
 router.post('/comment', async function (req, res) {
   const { idPost, content, idUser, response } = req.body;
-  if (typeof idPost !== 'string' || typeof content !== 'string' || typeof idUser !== 'string')
+  if (typeof idPost !== 'string' || typeof content !== 'string' || typeof idUser === undefined)
     return res.status(400).send({ error: true });
 
   const comment = await createComment(Number(idPost), Number(idUser), null, content);
+  generateCommentNotifs(idPost, idUser, content);
   res.send(comment);
 });
 
